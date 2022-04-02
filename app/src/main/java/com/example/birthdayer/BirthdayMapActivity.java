324b8080dfb550f81674392f985250f1c5833fa5
JulianRebel <63709758+JulianRebel@users.noreply.github.com>
2022-04-02 13:52:03 +0200

    Map now uses user's location

--- app/src/main/java/com/example/birthdayer/BirthdayMapActivity.java
+++ app/src/main/java/com/example/birthdayer/BirthdayMapActivity.java
@@ -3,7 +3,16 @@ package com.example.birthdayer;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.mapbox.mapboxsdk.Mapbox;
@@ -14,19 +23,40 @@ import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import com.mapbox.mapboxsdk.maps.Style;
 
-public class BirthdayMapActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class BirthdayMapActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
 
     private MapView mapView;
-    private MapboxMap map;
+    private LocationManager locationManager;
+    private double latitude;
+    private double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
         setContentView(R.layout.activity_birthday_map);
+
+        getLocation();
+
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLocation() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String bestProvider = String.valueOf(locationManager.getBestProvider(criteria, true)).toString();
+
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        if (location != null) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+        } else{
+            locationManager.requestLocationUpdates(bestProvider, 1000, 0, this);
+        }
     }
 
     @Override
@@ -39,10 +69,19 @@ public class BirthdayMapActivity extends AppCompatActivity implements OnMapReady
                         "Map styles are ready", Toast.LENGTH_SHORT);
                 toast.show();
 
-                mapboxMap.setCameraPosition(new CameraPosition.Builder().target(new LatLng(37.7749,-122.4194)).zoom(15).build());
+                mapboxMap.setCameraPosition(new CameraPosition.Builder().target(new LatLng(latitude,longitude)).zoom(15).build());
             }
         });
+    }
 
+    @Override
+    public void onLocationChanged(Location location) {
+
+        locationManager.removeUpdates(this);
+
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        mapView.getMapAsync(this);
     }
 
     @Override
