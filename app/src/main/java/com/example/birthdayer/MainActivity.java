3bd5f10ccaba9446ac61665e4ce3db094934831e
Stripmaker24 <michelle.stripmaker24@hotmail.com>
2022-04-04 11:02:22 +0200

    final push

--- app/src/main/java/com/example/birthdayer/MainActivity.java
+++ app/src/main/java/com/example/birthdayer/MainActivity.java
@@ -10,58 +10,42 @@ import androidx.core.content.ContextCompat;
 import androidx.preference.PreferenceManager;
 
 import android.Manifest;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.UiModeManager;
-import android.content.Context;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.os.AsyncTask;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.CalendarContract;
 import android.provider.Settings;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.google.gson.Gson;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.List;
-import java.util.prefs.Preferences;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
-    private UiModeManager uiModeManager;
     private final ActivityResultLauncher<String> requestPermissionLauncher =
-        registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
-            if (isGranted) {
-                Log.i("Permission", "Granted");
-            } else {
-                Log.i("Permission", "Denied");
-            }
-    });
-    void setMode(){
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    Timber.i("Granted");
+                } else {
+                    Timber.i("Denied");
+                }
+            });
+
+    void setMode() {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         boolean darkmode = preferences.getBoolean("is_nightmode", true);
-        if(darkmode){
+        if (darkmode) {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
         } else {
             AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         }
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        uiModeManager = (UiModeManager) getSystemService(UI_MODE_SERVICE);
         setMode();
     }
 
@@ -81,12 +65,12 @@ public class MainActivity extends AppCompatActivity {
     public void toMapScreen(View view) {
         Intent intent = new Intent(this, BirthdayMapActivity.class);
 
-        if(requestLocationPermission()){
+        if (requestLocationPermission()) {
             startActivity(intent);
         }
     }
 
-    private boolean requestLocationPermission(){
+    private boolean requestLocationPermission() {
         if (ContextCompat.checkSelfPermission(
                 this, Manifest.permission.ACCESS_FINE_LOCATION) ==
                 PackageManager.PERMISSION_GRANTED) {
@@ -94,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
         } else if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
             ActivityCompat.requestPermissions(this, new String[]{
                     Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION,},1);
+                    Manifest.permission.ACCESS_COARSE_LOCATION,}, 1);
             return false;
         } else {
             requestPermissionLauncher.launch(
@@ -103,11 +87,12 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void toListScreen(View view){
+    public void toListScreen(View view) {
         Intent intent = new Intent(this, BirthdayListActivity.class);
         startActivity(intent);
     }
 
+    @SuppressLint("QueryPermissionsNeeded")
     public void openLocationSettings(View view) {
         Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
         if (intent.resolveActivity(getPackageManager()) != null) {
